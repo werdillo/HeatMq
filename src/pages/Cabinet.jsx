9b8c6a44b36612bfa1df5e9ca8c70609a352854f
werdillo <55383654+werdillo@users.noreply.github.com>
2023-02-20 00:12:53 +0200

    light control

--- src/pages/Cabinet.jsx
+++ src/pages/Cabinet.jsx
@@ -1,6 +1,17 @@
+import { createSignal } from "solid-js"
 import "../assets/scss/_cabinet.scss"
 
 export default function Cabinet() {
+	const [lux, setLux] = createSignal(350);
+	const start = '8:30';
+	const end = '18:30';
+
+
+	const plus = () => setLux(lux() + 10);
+	const minus = () => setLux(lux() - 10);
+
+	
+
 	return <>
 		<div class='cabinet'>
 			<div class='-container'>
@@ -26,15 +37,33 @@ export default function Cabinet() {
 							LUX
 						</div>
 						<div class="-text xl">
-							350
+							{lux}
 						</div>
 					</div>
 					<div class="-item">
 						<div class="-text">
-							Water level (mm)
+							Water level
 						</div>
 						<div class="-text xl">
 							50
+						</div>
+					</div>
+				</div>
+				<div class='-light-settings'>
+					<div class='-item'>
+						<div class='-title'>Set light working time</div> 
+						<div class='-timer'>
+							<input class="-input" value={start}/>
+							<div class="-txt">-</div>
+							<input class="-input" value={end} />
+						</div>
+					</div>
+					<div class='-item'>
+						<div class='-title'>Set lights power (+-10 lux )</div> 
+						<div class='-lux'>
+							<button class="-btn" onClick={minus}>-</button>
+							<div class="-txt">{lux}</div>
+							<button class="-btn" onClick={plus}>+</button>
 						</div>
 					</div>
 				</div>
